afb1309da226f77bedff7569b4b7ffa3bf399c8e
infrapale <infrapale@gmail.com>
2021-05-29 06:59:01 +0300

    Reading messages corrected to use the length information

--- rfm69_support.cpp
+++ rfm69_support.cpp
@@ -1,6 +1,6 @@
 
 #include "rfm69_support.h"
-#include <Secret.h>
+#include <secrets.h>
 
 //RH_RF69 rf69(RFM69_CS, RFM69_IRQN);
 static rfm69_struct rfm69;
@@ -40,8 +40,8 @@ void radio_init(byte rfm_cs_pin, byte rfm_irqn_pin, byte rfm_rst_pin,float rfm_f
      // ishighpowermodule flag set like this:
      rfm69.rfm->setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
 
-     // in secret.h          1234567890123456
-     //uint8_t rfm69_key[] ="Xyzabde123456789"; //exactly the same 16 characters/bytes on all nodes!
+     // in secrets.h          1234567890123456
+     uint8_t rfm69_key[] = RFM69_KEY; //exactly the same 16 characters/bytes on all nodes!
      rfm69.rfm->setEncryptionKey(rfm69_key);
      Serial.print("RFM69 rf69 @");  Serial.print((int)rfm_freq,DEC);  Serial.println(" MHz");
     
@@ -66,18 +66,19 @@ uint8_t radio_read_msg(char *inp_buf, uint8_t max_len){
    boolean result = false;
    if (radio_check_available_msg()) {
        if (rfm69.rfm->recv(rd_buf, &len)) {
-           //Serial.print("len="); Serial.println(len);
+           Serial.print("len="); Serial.println(len);
            uint8_t i = 0;
            do {
                inp_buf[i] = (char)rd_buf[i];
                i++;
-            } while (rd_buf[i] != 0 && (i < max_len));
+            //} while (rd_buf[i] != 0 && (i < max_len));
+			} while ((i < len) && (i < max_len));
             inp_buf[i] = 0; rd_buf[i] = 0; 
             len = i;
             
             if (len> 0){
-                // Serial.print("Received ["); Serial.print(len); Serial.print("]: ");
-                // Serial.println((char*)rd_buf); Serial.print("RSSI: "); Serial.println(rfm69.rfm->lastRssi(), DEC);
+                Serial.print("Received ["); Serial.print(len); Serial.print("]: ");
+                Serial.println((char*)rd_buf); Serial.print("RSSI: "); Serial.println(rfm69.rfm->lastRssi(), DEC);
            }
        }       
    }
